2000790921677781d2ae8045e62870917ee1a400
EpicYen-25 <119719082+EpicYen-25@users.noreply.github.com>
2022-12-17 11:58:22 +0800

    Upload files

---
+++ Ping-Pong.py
@@ -0,0 +1,80 @@
+from pygame import *
+from random import randint
+
+#parent class for other sprites
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, sixe_y, player speed):
+        super(). __init__()
+
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        self.rect = self.image.ger_rect()
+        self.rect.x = player_x
+        self.rext.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#child class for player
+class Paddle (GameSprite):
+    def update_right(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+    def update_left(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+#interface
+BLUE = (200, 255, 255)
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(BLUE)
+
+#sprites
+red_img = "red.png"
+blue_img = "paddle_blue.png"
+ball_img = "ball"
+
+paddleLeft = Paddle (blue_img, 20, 200, 30, 150, 150)
+paddleRight = Paddle (red_img, 650, 200, 30, 150, 150)
+ball = GameSprite(ball_img, 330, 200, 50, 50, 50)
+
+game = True
+finish = False
+
+#fonts
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type ==QUIT:
+            game = False
+
+    if finish != True:
+        window.fill (BLUE)
+        paddleLeft.update_left()
+        paddleRight.update_right()
+
+        ball.rect.x += speed_x
+        ball.rect.x += speed_y
+
+        paddleLeft.reset()
+        paddleRight.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
--- README.md
+++ README.md
@@ -3,16 +3,16 @@ A classic multiplayer game.
 
 CONTROLS AND BUTTONS:
 
-- For Player #1 (on the left), the controls used to move are the W and S keys.
-- For Player #2 (on the right), the controls used to move are the up and down arrow keys.
+    - For Player #1 (on the left), the controls used to move are the W and S keys.
+    - For Player #2 (on the right), the controls used to move are the up and down arrow keys.
 
 
 BALL MOVEMENT & ACTIVITY:
 
-- The ball moves automatically, it will bounce of the bottom wall and the paddle.
-- The opponent will gain points if the ball hits the left or right wall (the goals).
+    - The ball moves automatically, it will bounce of the bottom wall and the paddle.
+    - The opponent will gain points if the ball hits the left or right wall (the goals).
 
 
 SCORING:
 
-- 20 scores are required to gain to win against an opponent.
+    - 20 scores are required to gain to win against an opponent.
